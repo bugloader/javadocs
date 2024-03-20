f8c92b2b3bb5b4086a06aed691e1831612ffa154
bugloader <36098023+bugloader@users.noreply.github.com>
2024-03-19 23:33:30 -0500

    update

---
+++ cs300hw6/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Item"},{"p":"<Unnamed>","l":"Packing"},{"p":"<Unnamed>","l":"PackingTester"},{"p":"<Unnamed>","l":"Position"},{"p":"<Unnamed>","l":"Suitcase"},{"p":"<Unnamed>","l":"Utilities"}];updateSearchResults();
